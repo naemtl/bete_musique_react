28f2143fc820ebdff434c14b92aaad2388341476
Matthew <mpstinis@gmail.com>
2020-12-26 23:10:49 -0500

    [Navbar] added audio player and controls, needs more work

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+import beteIcon from "../../assets/images/bete-white-bg.jpg";
+import playBtn from "../../assets/images/play-bsn.png";
+import pauseBtn from "../../assets/images/pause-bsn.png";
+import './styles.css'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='navbar-container'>
+            <div className='icon-container navbar-item'>
+                <Link to="/">
+                    <img src={beteIcon} alt="bete-icon" />
+                </Link>
+            </div>
+            {/* <audio autoPlay id="audio" src="http://betemusique.out.airtime.pro:8000/betemusique_a"/> */}
+            <AudioPlayer
+                className='audio-player'
+                src="http://betemusique.out.airtime.pro:8000/betemusique_a"
+                showJumpControls={false}
+                showFilledProgress={false}
+                customProgressBarSection={[]}
+                customControlsSection={['MAIN_CONTROLS', 'VOLUME_CONTROLS']}
+                // customIcons={{
+                //     play: playBtn,
+                //     pause: pauseBtn
+                // }}
+            // other props here
+            />
+
         </div>
     )
 }
